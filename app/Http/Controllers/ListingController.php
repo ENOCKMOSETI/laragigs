d15e7f948361626bcc8aaa229d07fe3228888877
MOSETI <enock.g.moseti@gmail.com>
2022-05-20 14:57:26 +0300

    update resource method names

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,14 +9,14 @@ class ListingController extends Controller
 {
     // Get all Listings
     public function index() {
-        return view('listings', [
+        return view('listings.index', [
             'listings' => Listing::all()
         ]);
     }
 
     // Get single Listing
     public function show(Listing $listing) {
-        return view('listing', [
+        return view('listings.show', [
             'listing' => $listing
         ]);
     }
