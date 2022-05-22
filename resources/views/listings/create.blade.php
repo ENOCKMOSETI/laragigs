5ca5e221e68dc539ac6dbb240e0c69d5df8aa7fa
MOSETI <enock.g.moseti@gmail.com>
2022-05-23 01:03:27 +0300

    persist form data on error

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -20,7 +20,7 @@
                     <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="company"
+                    name="company" value="{{old('company')}}"
                     />
                     @error('company')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
@@ -36,7 +36,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
-                    />
+                    value="{{old('title')}}"/>
                     @error('title')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
                     @enderror
@@ -53,7 +53,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
-                    />
+                    value="{{old('location')}}"/>
                     @error('location')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
                     @enderror
@@ -66,7 +66,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="email"
+                    name="email" value="{{old('email')}}"
                     />
                     @error('email')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
@@ -83,7 +83,7 @@
                 <input
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
-                    name="website"
+                    name="website" value="{{old('website')}}"
                 />
                 @error('website')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
@@ -99,7 +99,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
-                />
+                    value="{{old('tags')}}"/>
                 @error('tags')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
                     @enderror
@@ -128,7 +128,7 @@
                     name="description"
                     rows="10"
                     placeholder="Include tasks, requirements, salary, etc"
-                ></textarea>
+                >{{old('tags')}}</textarea>
                 @error('description')
                         <p class="text-red-500 text-xs-mt-1">{{$message}}</p>
                     @enderror
